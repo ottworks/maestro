b25cc48d7fca43047b935d3f06a75bab69e74dbc
DaaOtt <daaott@gmail.com>
2015-05-19 21:23:04 -0700

    ignite
    fixed #7

---
+++ lua/maestro/plugins/sh_ignite.lua
@@ -0,0 +1,12 @@
+maestro.command("ignite", {"player:target", "number:time"}, function(caller, targets, time)
+	if #targets == 0 then
+		return true, "Query matched no players."
+	end
+	for _, ply in pairs(targets) do
+		ply:Ignite(time)
+	end
+	if time then
+		return false, "ignited %1 for %2 seconds"
+	end
+	return false, "ignited %1"
+end)
