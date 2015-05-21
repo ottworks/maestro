1895e7856f0ca5b73663714706e4fef8b0277b1e
DaaOtt <daaott@gmail.com>
2015-05-20 21:24:19 -0700

    tp

---
+++ lua/maestro/plugins/sh_tp.lua
@@ -0,0 +1,22 @@
+maestro.command("tp", {"player:target(optional)"}, function(caller, targets)
+	targets = targets or {}
+	if #targets > 1 then
+		return true, "Query matched more than 1 player."
+	end
+	local tr = util.TraceHull{
+		start = caller:GetShootPos(),
+		endpos = caller:GetShootPos() + caller:EyeAngles():Forward() * 16384,
+		mins = Vector(-16, -16, 0),
+		maxs = Vector(16, 16, 72),
+		filter = {caller, targets[1]},
+	}
+	if not tr.HitPos then
+		return true, "No room!"
+	end
+	if #targets == 1 then
+		targets[1]:SetPos(tr.HitPos)
+		return false, "teleported %1"
+	end
+	caller:SetPos(tr.HitPos)
+	return false, "teleported themselves"
+end)
