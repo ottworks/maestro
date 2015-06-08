4cbb2e451b763d98f91c6e9cfd9d7e066ec99a07
DaaOtt <daaott@gmail.com>
2015-06-08 15:17:50 -0700

    More robust time translation
    (asdf)[http://i.imgur.com/oQIfZne.png]

--- lua/maestro/sh_time.lua
+++ lua/maestro/sh_time.lua
@@ -3,11 +3,17 @@ TIME_MINUTE = TIME_SECOND * 60
 TIME_HOUR = TIME_MINUTE * 60
 TIME_DAY = TIME_HOUR * 24
 TIME_WEEK = TIME_DAY * 7
+TIME_FORTNIGHT = TIME_WEEK * 2
 TIME_MONTH = TIME_DAY * (365.2425/12)
+TIME_QUARTER = TIME_MONTH * 3
 TIME_YEAR = TIME_DAY * 365.2425
 TIME_DECADE = TIME_YEAR * 10
 TIME_CENTURY = TIME_DECADE * 10
 TIME_MILLENNIUM = TIME_CENTURY * 10
+TIME_AGE = TIME_YEAR * 1000000
+TIME_EPOCH = TIME_AGE * 10
+TIME_ERA = TIME_EPOCH * 10
+TIME_AEON = TIME_ERA * 10
 
 local function plural(a, n)
 	if n == 1 then
@@ -36,21 +42,37 @@ function maestro.toseconds(str)
 			if u1 ~= u2 and not num then
 				error("invalid string")
 			end
-			local u = string.lower(str:sub(u1, u2))
+			local u = str:sub(u1, u2)
 			if u == "s" then
 				total = total + num
+			elseif u == "m" then
+				total = total + num * TIME_MINUTE
 			elseif u == "h" then
 				total = total + num * TIME_HOUR
 			elseif u == "d" then
 				total = total + num * TIME_DAY
 			elseif u == "w" then
 				total = total + num * TIME_WEEK
-			elseif u == "m" then
+			elseif u == "f" then
+				total = total + num * TIME_FORTNIGHT
+			elseif u == "M" then
 				total = total + num * TIME_MONTH
+			elseif u == "q" then
+				total = total + num * TIME_QUARTER
 			elseif u == "y" then
 				total = total + num * TIME_YEAR
+			elseif u == "D" then
+				total = total + num * TIME_DECADE
 			elseif u == "c" then
 				total = total + num * TIME_CENTURY
+			elseif u == "a" then
+				total = total + num * TIME_AGE
+			elseif u == "e" then
+				total = total + num * TIME_EPOCH
+			elseif u == "E" then
+				total = total + num * TIME_ERA
+			elseif u == "A" then
+				total = total + num * TIME_AEON
 			end
 			num = nil
 			cursor = u2
@@ -61,7 +83,7 @@ function maestro.toseconds(str)
 		end
 	end
 	if num then
-		total = total + num * TIME_MINUTE
+		total = total + num
 	end
 	return total
 end
@@ -73,7 +95,23 @@ function maestro.time(num, limit)
 	end
 	local ret = {}
 	while not limit or limit > 0 do
-		if num >= TIME_MILLENNIUM then
+		if num >= TIME_AEON then
+			local c = math.floor(num / TIME_AEON)
+			ret[#ret + 1] = c .. " " .. plural("aeon", c)
+			num = num - TIME_AEON * c
+		elseif num >= TIME_ERA then
+			local c = math.floor(num / TIME_ERA)
+			ret[#ret + 1] = c .. " " .. plural("era", c)
+			num = num - TIME_ERA * c
+		elseif num >= TIME_EPOCH then
+			local c = math.floor(num / TIME_EPOCH)
+			ret[#ret + 1] = c .. " " .. plural("epoch", c)
+			num = num - TIME_EPOCH * c
+		elseif num >= TIME_AGE then
+			local c = math.floor(num / TIME_AGE)
+			ret[#ret + 1] = c .. " " .. plural("age", c)
+			num = num - TIME_AGE * c
+		elseif num >= TIME_MILLENNIUM then
 			local c = math.floor(num / TIME_MILLENNIUM)
 			ret[#ret + 1] = c .. " " .. plural("millennium", c)
 			num = num - TIME_MILLENNIUM * c
