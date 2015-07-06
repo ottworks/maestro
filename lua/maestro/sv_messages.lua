c81d1217b8d26bbf9d49ae42d3d2af7f5ea3edff
DaaOtt <daaott@gmail.com>
2015-07-06 11:11:11 -0700

    Smarter join/leave messages

--- lua/maestro/sv_messages.lua
+++ lua/maestro/sv_messages.lua
@@ -1,9 +1,18 @@
 hook.Add("CheckPassword", "maestro_messages", function(id64, ip, sv, cl, name)
-	maestro.chat(nil, Color(255, 255, 255), "Player ", Color(78, 196, 255), name, Color(255, 255, 255), " (", Color(78, 196, 255), util.SteamIDFrom64(id64), Color(255, 255, 255), ") has connected to the server.")
+	local id = util.SteamIDFrom64(id64)
+	local ban = maestro.bans[id]
+	if ban and ban.unban > os.time() then
+		return
+	end
+	if sv and sv ~= cl then
+		maestro.chat(nil, Color(255, 255, 255), "Player ", Color(78, 196, 255), name, Color(255, 255, 255), " (", Color(78, 196, 255), util.SteamIDFrom64(id64), Color(255, 255, 255), ") tried to connect to the server with incorrect password \"", cl, "\".")
+	else
+		maestro.chat(nil, Color(255, 255, 255), "Player ", Color(78, 196, 255), name, Color(255, 255, 255), " (", Color(78, 196, 255), util.SteamIDFrom64(id64), Color(255, 255, 255), ") has connected to the server.")
+	end
 end)
 hook.Add("PlayerInitialSpawn", "maestro_messages", function(ply)
-	maestro.chat(nil, Color(255, 255, 255), "Player ", Color(78, 196, 255), ply:Nick(), Color(255, 255, 255), " (", Color(78, 196, 255), ply:SteamID(), Color(255, 255, 255), ") has joined the server.")
+	maestro.chat(nil, Color(255, 255, 255), "Player ", Color(78, 196, 255), ply:Nick(), Color(255, 255, 255), " (", Color(78, 196, 255), ply:SteamID(), Color(255, 255, 255), ") has joined the game.")
 end)
 hook.Add("PlayerDisconnected", "maestro_messages", function(ply)
-	maestro.chat(nil, Color(255, 255, 255), "Player ", Color(78, 196, 255), ply:Nick(), Color(255, 255, 255), " (", Color(78, 196, 255), ply:SteamID(), Color(255, 255, 255), ") has left the server.")
+	maestro.chat(nil, Color(255, 255, 255), "Player ", Color(78, 196, 255), ply:Nick(), Color(255, 255, 255), " (", Color(78, 196, 255), ply:SteamID(), Color(255, 255, 255), ") has left the game.")
 end)
