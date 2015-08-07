8af69a5b1791cf18cce3bbfd871be78268d3731f
DaaOtt <daaott@gmail.com>
2015-08-07 12:41:23 -0700

    ent
    fixes #17

--- lua/maestro/plugins/sh_rcon.lua
+++ lua/maestro/plugins/sh_rcon.lua
@@ -13,3 +13,45 @@ maestro.command("lua", {"string:lua"}, function(caller, ...)
 end, [[
 Runs Lua on the server.
 Include your code in double quotes to prevent malformation.]])
+maestro.command("ent", {"class", "keyvalues(multiple)"}, function(caller, class, ...)
+	if not caller then
+		return true, "You cannot create an entity from the console!"
+	elseif not class then
+		return true, "Invalid class!"
+	end
+	local params = {...}
+	local ent = ents.Create(class)
+	if not IsValid(ent) then
+		return true, "Invalid entity \"" .. class .. "\"."
+	end
+	ent:SetPos(caller:GetEyeTrace().HitPos + Vector(0, 0, 25))
+
+	for i = 1, #params do
+		if tonumber(params[i]) then
+			ent:AddFlags(tonumber(params[i]))
+		else
+			local key, value = string.match(params[i], "([^:]+):([^:]+)")
+			if not key or not value then
+				ent:Remove()
+				return true, "Invalid keyvalue pair \"" .. params[i] .. "\". Keyvalues are colon separated."
+			end
+			ent:SetKeyValue(key, value)
+		end
+	end
+
+	ent:Spawn()
+	ent:Activate()
+
+	undo.Create("ms ent")
+		undo.AddEntity(ent)
+		undo.SetPlayer(caller)
+	undo.Finish()
+	if #params > 0 then
+		return false, "created ent %1 with params %%"
+	end
+	return false, "created ent %1"
+end, [[
+Creates an entity and sets properties on it.
+Keyvalues are formatted as such:
+key:value
+Flags are numbers.]])
