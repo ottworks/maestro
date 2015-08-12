f03fa6fa4014b973d19229bb238edd7d2c607a10
DaaOtt <daaott@gmail.com>
2015-08-12 00:10:39 -0700

    why would you ever need to gag on a time limit

--- lua/maestro/plugins/sh_gag.lua
+++ lua/maestro/plugins/sh_gag.lua
@@ -4,32 +4,24 @@ if SERVER then
 		return gagged[talker] and false
 	end)
 end
-maestro.command("gag", {"player:target", "time:optional"}, function(caller, targets, time)
-	if #targets < 1 then
-		return true, "Query matched no players."
-	end
-	for i = 1, #targets do
-		gagged[targets[i]] = true
-		if time then
-			timer.Create("maestro_gag_" .. targets[i]:EntIndex(), time, 1, function()
-				gagged[targets[i]] = nil
-			end)
-		end
-	end
-	if time then
-		return false, "gagged %1 for %2"
-	end
-	return false, "gagged %1"
-end, [[
-Prevents the targetted players from using voice chat for an optional amount of time.]])
-maestro.command("ungag", {"player:target"}, function(caller, targets, time)
-	if #targets < 1 then
-		return true, "Query matched no players."
-	end
-	for i = 1, #targets do
-		gagged[targets[i]] = nil
-		timer.Remove("maestro_gag_" .. targets[i]:EntIndex())
-	end
-	return false, "ungagged %1"
-end, [[
-Re-enables the targetted players' voice chats.]])
+maestro.command("gag", {"player:target", "boolean:state(optional)"}, function(caller, targets, state)
+    if #targets == 0 then
+        return true, "Query matched no players."
+    end
+    if state == nil then
+        for i = 1, #targets do
+            local ply = targets[i]
+            gagged[ply] = not gagged[ply]
+        end
+        return false, "toggled gag on %1"
+    else
+        for i = 1, #targets do
+            local ply = targets[i]
+            gagged[ply] = state
+        end
+        if state then
+            return false, "gagged %1"
+        end
+        return false, "ungagged %1"
+    end
+end)
