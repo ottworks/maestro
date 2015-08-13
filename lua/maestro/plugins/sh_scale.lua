d851d82751e88b181621953fa911018a11dcd639
DaaOtt <daaott@gmail.com>
2015-08-13 12:35:32 -0700

    Correct walking animations for scaled players

--- lua/maestro/plugins/sh_scale.lua
+++ lua/maestro/plugins/sh_scale.lua
@@ -46,7 +46,7 @@ end
 if SERVER then
     util.AddNetworkString("maestro_scale")
 end
-maestro.command("scale", {"player:target", "number:scale"}, function(caller, targets, scale)
+maestro.command("scale", {"player:target", "number:scale(optional)"}, function(caller, targets, scale)
     if #targets == 0 then
         return true, "Query matched no players."
     end
@@ -120,5 +120,58 @@ maestro.hook("EntityEmitSound", "scale", function(info)
             info.Pitch = math.Clamp(info.Pitch / scaledata.scaled[ply]^(1/3), 0, 255)
             return true
         end
+    end
+end)
+maestro.hook("UpdateAnimation", "scale", function(ply, velocity, maxseqgroundspeed)
+    if scaledata.scaled[ply] then
+        local len = velocity:Length() / scaledata.scaled[ply]
+    	local movement = 1.0
+
+    	if ( len > 0.2 ) then
+    		movement = ( len / maxseqgroundspeed )
+    	end
+
+    	local rate = math.min( movement, 2 )
+
+    	-- if we're under water we want to constantly be swimming..
+    	if ( ply:WaterLevel() >= 2 ) then
+    		rate = math.max( rate, 0.5 )
+    	elseif ( !ply:IsOnGround() && len >= 1000 ) then
+    		rate = 0.1
+    	end
+
+    	ply:SetPlaybackRate( rate )
+
+    	if ( ply:InVehicle() ) then
+
+    		local Vehicle = ply:GetVehicle()
+
+    		-- We only need to do this clientside..
+    		if ( CLIENT ) then
+    			--
+    			-- This is used for the 'rollercoaster' arms
+    			--
+    			local Velocity = Vehicle:GetVelocity()
+    			local fwd = Vehicle:GetUp()
+    			local dp = fwd:Dot( Vector( 0, 0, 1 ) )
+    			local dp2 = fwd:Dot( Velocity )
+
+    			ply:SetPoseParameter( "vertical_velocity", ( dp < 0 and dp or 0 ) + dp2 * 0.005 )
+
+    			-- Pass the vehicles steer param down to the player
+    			local steer = Vehicle:GetPoseParameter( "vehicle_steer" )
+    			steer = steer * 2 - 1 -- convert from 0..1 to -1..1
+    			if ( Vehicle:GetClass() == "prop_vehicle_prisoner_pod" ) then steer = 0 ply:SetPoseParameter( "aim_yaw", math.NormalizeAngle( ply:GetAimVector():Angle().y - Vehicle:GetAngles().y - 90 ) ) end
+    			ply:SetPoseParameter( "vehicle_steer", steer )
+
+    		end
+
+    	end
+
+    	if ( CLIENT ) then
+    		GAMEMODE:GrabEarAnimation( ply )
+    		GAMEMODE:MouthMoveAnimation( ply )
+    	end
+        return true
     end
 end)
