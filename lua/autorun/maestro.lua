ba60b28c44661457ec0ebab0bd5200069accf618
DaaOtt <daaott@gmail.com>
2015-08-17 15:23:59 -0700

    Update notifications

--- lua/autorun/maestro.lua
+++ lua/autorun/maestro.lua
@@ -1,10 +1,11 @@
+local version = "1.2.2"
 maestro = {}
-
 print("\201\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\187")
-print("\186 Maestro 1.2.1                    \186")
+print("\186 Maestro " .. version .. string.rep(" ", 25 - #version) .. "\186")
 print("\186     (it's pronounced \"my strow\") \186")
 print("\186 (c) 2015 Ott(STEAM_0:0:36527860) \186")
 print("\199\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\182")
+hook.Call("maestro_preload")
 local files, folders = file.Find("maestro/*", "LUA")
 table.sort(files, function(a, b)
 	local suba = string.sub(a, 1, 3)
@@ -34,8 +35,9 @@ for k, v in pairs(files) do
 		end
 	end
 end
-hook.Call("maestro_prepluginload")
+hook.Call("maestro_postload")
 print("\199\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\196\182")
+hook.Call("maestro_prepluginload")
 local files, folders = file.Find("maestro/plugins/*", "LUA")
 table.sort(files, function(a, b)
 	local suba = string.sub(a, 1, 3)
@@ -65,5 +67,50 @@ for k, v in pairs(files) do
 		end
 	end
 end
-print("\200\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\188")
 hook.Call("maestro_postpluginload")
+print("\200\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\188")
+
+hook.Add("InitPostEntity", "maestro_updatecheck", function()
+	timer.Simple(0, function()
+		http.Fetch("https://raw.githubusercontent.com/DaaOtt/maestro/master/lua/autorun/maestro.lua", function(body)
+			local str = string.match(body, "[^\n]+")
+			local ver = str:sub(18, -2)
+			local major, minor, patch = ver:match("(%d+)%.(%d+)%.(%d+)")
+			major = tonumber(major) or 0
+			minor = tonumber(minor) or 0
+			patch = tonumber(patch) or 0
+			local curmajor, curminor, curpatch = version:match("(%d+)%.(%d+)%.(%d+)")
+			curmajor = tonumber(curmajor) or 0
+			curminor = tonumber(curminor) or 0
+			curpatch = tonumber(curpatch) or 0
+			local msg
+			if major > curmajor then
+				msg = [[
+A new major version of Maestro is
+available. Note that new major
+versions break compatibility with
+previous major versions.
+]]
+			elseif minor > curminor then
+				msg = [[
+A new minor version of Maestro is
+available. Minor versions add a
+new feature or mechanismsm.
+]]
+			elseif patch > curpatch then
+				msg = [[
+A new patch is available for
+Maestro. Patches offer fixes bugs
+and errors.
+]]
+			end
+			if msg then
+				print("\201\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\187")
+				for w in string.gmatch(msg, "[^\n]+") do
+					print("\186" .. w .. string.rep(" ", 34 - #w) .. "\186")
+				end
+				print("\200\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\205\188")
+			end
+		end)
+	end)
+end)
