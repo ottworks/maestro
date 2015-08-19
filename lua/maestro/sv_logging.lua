414e92a3876d20a41158211ee1ad296d78e761a3
DaaOtt <daaott@gmail.com>
2015-08-18 21:10:52 -0700

    Fixed a typo/bug in logging

--- lua/maestro/sv_logging.lua
+++ lua/maestro/sv_logging.lua
@@ -1,8 +1,8 @@
-maestro.hook("PlayerDeath", "logging", function(victime, inflictor, attacker)
+maestro.hook("PlayerDeath", "logging", function(victim, inflictor, attacker)
     local atk = tostring(attacker)
     if IsValid(attacker) and attacker:IsPlayer() then
         atk = attacker:Nick() .. "(" .. attacker:SteamID() .. ")"
     end
     local txt = "was killed by " .. atk .. " with a(n) " .. tostring(inflictor)
-    maestro.log("log_" .. os.date("%y-%m-%d"), os.date("[%H:%M] ") .. ply:Nick() .. "(" .. ply:SteamID() .. "): " .. txt)
+    maestro.log("log_" .. os.date("%y-%m-%d"), os.date("[%H:%M] ") .. victim:Nick() .. "(" .. victim:SteamID() .. "): " .. txt)
 end)
