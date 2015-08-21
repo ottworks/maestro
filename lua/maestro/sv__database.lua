e015462336ffa6dfa7a4ba24b522d8642f2ec5a3
DaaOtt <daaott@gmail.com>
2015-08-20 19:19:15 -0700

    Added support for support for alternate storage

--- lua/maestro/sv__database.lua
+++ lua/maestro/sv__database.lua
@@ -2,35 +2,81 @@ if not file.Exists("maestro", "DATA") then
 	file.CreateDir("maestro")
 end
 
+--[[
+	Use these functions to define custom save/load operations. Note that your
+	data plugin should be in lua/maestro and should be named so that it comes
+	alphabetically after sv__database and before sv__zzzz, as later files use
+	database functionality.
+
+	Example code:
+	maestro.definedataproc("ranks", function(name, tab)
+		saveSomeThingOrMaybeConvertItToADifferentStructureTheChoiceIsYours(tab)
+	end, function(name)
+		local tab = loadSomeThingOrDoSomethingMoreComplexIDontKnowYouDontNeedNoMan(name)
+		return tab
+	end)
+--]]
+local dataprocs = {}
+function maestro.definedataproc(type, save, load)
+	dataprocs[type] = {
+		save = save,
+		load = load,
+	}
+end
+
+local logprocs = {}
+function maestro.definelogproc(type, log, read)
+	logprocs[type] = {
+		log = log,
+		read = read,
+	}
+end
+
 function maestro.load(name)
-	local newfile = false
-	if not file.Exists("maestro/" .. name .. ".txt", "DATA") then
-		file.Write("maestro/" .. name .. ".txt", "")
-		newfile = true
+	if dataprocs[name] then
+		return dataprocs[name].load(name)
+	else
+		local newfile = false
+		if not file.Exists("maestro/" .. name .. ".txt", "DATA") then
+			file.Write("maestro/" .. name .. ".txt", "")
+			newfile = true
+		end
+		return util.JSONToTable(file.Read("maestro/" .. name .. ".txt")) or {}, newfile
 	end
-	return util.JSONToTable(file.Read("maestro/" .. name .. ".txt")) or {}, newfile
 end
 
 function maestro.save(name, tab)
-	file.Write("maestro/" .. name .. ".txt", util.TableToJSON(tab))
+	if dataprocs[name] then
+		return dataprocs[name].save(name, tab)
+	else
+		file.Write("maestro/" .. name .. ".txt", util.TableToJSON(tab))
+	end
 end
 
 function maestro.log(name, item)
-	if type(item) == "table" then item = util.TableToJSON(item) end
-	if not file.Exists("maestro/" .. name .. ".txt", "DATA") then
-		file.Write("maestro/" .. name .. ".txt", "")
+	if logprocs[name] then
+		logprocs[name].log(name, item)
+	else
+		if type(item) == "table" then item = util.TableToJSON(item) end
+		if not file.Exists("maestro/" .. name .. ".txt", "DATA") then
+			file.Write("maestro/" .. name .. ".txt", "")
+		end
+		file.Append("maestro/" .. name .. ".txt", item .. "\n")
 	end
-	file.Append("maestro/" .. name .. ".txt", item .. "\n")
 end
 
 function maestro.read(name, iterator)
-	local ret = {}
-	if iterator then
-		return string.gmatch(file.Read("maestro/" .. name .. ".txt"), "[^\n]+")
-	end
+	if logprocs[name] then
+		return logprocs[name].read(name, iterator)
+	else
+		local ret = {}
+		if iterator then
+			return string.gmatch(file.Read("maestro/" .. name .. ".txt"), "[^\n]+")
+		end
 
-	for w in string.gmatch(file.Read("maestro/" .. name .. ".txt"), "[^\n]+") do
-		ret[#ret + 1] = w
+		for w in string.gmatch(file.Read("maestro/" .. name .. ".txt"), "[^\n]+") do
+			ret[#ret + 1] = w
+		end
+		return ret
 	end
-	return ret
 end
