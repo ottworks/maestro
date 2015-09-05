a6e40702773315d5ed1f728741bbe137fe7300a4
DaaOtt <daaott@gmail.com>
2015-09-04 23:25:19 -0700

    Made slap kick players out of vehicles

--- lua/maestro/plugins/sh_slap.lua
+++ lua/maestro/plugins/sh_slap.lua
@@ -1,3 +1,15 @@
+local function slap(ply)
+	if IsValid(ply:GetVehicle()) then
+		ply:ExitVehicle()
+	end
+	local info = DamageInfo()
+	info:SetAttacker(Entity(0))
+	info:SetDamage(dmg)
+	info:SetDamageType(DMG_CLUB)
+	ply:TakeDamageInfo(info)
+	ply:SetVelocity(Vector(math.random(-100, 100), math.random(-100, 100), math.random(250, 300)))
+	ply:EmitSound("physics/body/body_medium_impact_hard6.wav", 60, math.random(90, 110))
+end
 maestro.command("slap", {"player:target", "number:damage(optional)", "number:times(optional)"}, function(caller, targets, dmg, times)
 	if not targets or #targets == 0 then
 		return true, "Query matched no players."
@@ -11,25 +23,13 @@ maestro.command("slap", {"player:target", "number:damage(optional)", "number:tim
 			continue
 		end
 		dmg = dmg or 0
-		local info = DamageInfo()
-		info:SetAttacker(Entity(0))
-		info:SetDamage(dmg)
-		info:SetDamageType(DMG_CLUB)
-		ply:TakeDamageInfo(info)
-		ply:SetVelocity(Vector(math.random(-100, 100), math.random(-100, 100), math.random(250, 300)))
-		ply:EmitSound("physics/body/body_medium_impact_hard6.wav", 60, math.random(90, 110))
+		slap(ply)
 		if times then
 			timer.Create("maestro_slap_" .. ply:EntIndex(), 1, times - 1, function()
 				if not ply:Alive() then
 					timer.Remove("maestro_slap_" .. ply:EntIndex())
 				end
-				local info = DamageInfo()
-				info:SetAttacker(Entity(0))
-				info:SetDamage(dmg)
-				info:SetDamageType(DMG_CLUB)
-				ply:TakeDamageInfo(info)
-				ply:SetVelocity(Vector(math.random(-100, 100), math.random(-100, 100), math.random(250, 300)))
-				ply:EmitSound("physics/body/body_medium_impact_hard6.wav", 60, math.random(90, 110))
+				slap(ply)
 			end)
 		end
 	end
