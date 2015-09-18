280ae02b0aa94bc17b7591f60cca9ef42647658c
DaaOtt <daaott@gmail.com>
2015-09-17 18:20:00 -0700

    oops

--- lua/maestro/plugins/sh_vac.lua
+++ lua/maestro/plugins/sh_vac.lua
@@ -1,9 +1,10 @@
 maestro.command("vac", {"player:target"}, function(caller, targets)
     if #targets == 0 then
-        return false, "Query matched no players."
+        return true, "Query matched no players."
     elseif #targets > 1 then
-        return false, "Query matched more than one player."
+        return true, "Query matched more than one player."
     end
     targets[1]:Kick("#VAC_ConnectionRefusedDetail")
+    return false, "VAC banned %1"
 end, [[
 Kicks a player with the VAC ban message. Fun!]])
