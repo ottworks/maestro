d7271084b055537e004bd4aac4d69cfe9d4d6572
DaaOtt <daaott@gmail.com>
2015-11-21 16:44:38 -0800

    Made rtv from console force a vote

--- lua/maestro/plugins/sh_maps.lua
+++ lua/maestro/plugins/sh_maps.lua
@@ -10,7 +10,7 @@ maestro.command("map", {"map"}, function(caller, map)
 	return false, "changed the level to %1"
 end)
 maestro.command("nominate", {"map"}, function(caller, map)
-	if not file.Exists("maps/" .. map .. ".bsp", "GAME") then
+	if not file.Exists("maps/" .. map .. ".bsp", "MOD") then
 		return true, "Map not found: " .. map .. ".bsp"
 	end
 	if nominated[map] then
@@ -20,17 +20,47 @@ maestro.command("nominate", {"map"}, function(caller, map)
 	return false, "nominated map %1"
 end)
 maestro.command("rtv", {}, function(caller)
-	local voted
-	if not rtv[caller] then
-		rtv[caller] = true
-		voted = true
-		rtv.count = (rtv.count or 0) + 1
-	end
-	local c = rtv.count
-	local t = #player.GetAll()
-	local p = c/t
-	local r = math.ceil(0.75 * t)
-	if c >= r then
+	if caller then
+		local voted
+		if not rtv[caller] then
+			rtv[caller] = true
+			voted = true
+			rtv.count = (rtv.count or 0) + 1
+		end
+		local c = rtv.count
+		local t = #player.GetAll()
+		local p = c/t
+		local r = math.ceil(0.75 * t)
+		if c >= r then
+			local maps = {}
+			for map in RandomPairs(nominated) do
+				if #maps >= 5 then break end
+				maps[#maps + 1] = map
+			end
+			local files = file.Find("maps/*.bsp", "MOD")
+			for _, map in RandomPairs(files) do
+				if #maps >= 5 then break end
+				maps[#maps + 1] = map:sub(1, -5)
+			end
+			maestro.vote("Vote for the next map!", maps, function(option, voted, total)
+				nominated = {}
+				rtv = {}
+				if option and file.Exists("maps/" .. option .. ".bsp", "MOD") then
+					maestro.chat(nil, Color(255, 255, 255), "Option \"", option, "\" has won. (", voted, "/", total, ")")
+					maestro.chat(nil, Color(255, 255, 255), "Map change in 3 seconds.")
+					timer.Simple(3, function()
+						RunConsoleCommand("changelevel", option)
+					end)
+				else
+					maestro.chat(nil, Color(255, 255, 255), "No options have won.")
+				end
+			end)
+		end
+		if voted then
+			return false, "wants to rock the vote (" .. c .. "/" .. r .. ")"
+		end
+		return true, "You have already rocked the vote!"
+	else
 		local maps = {}
 		for map in RandomPairs(nominated) do
 			if #maps >= 5 then break end
@@ -55,8 +85,4 @@ maestro.command("rtv", {}, function(caller)
 			end
 		end)
 	end
-	if voted then
-		return false, "wants to rock the vote (" .. c .. "/" .. r .. ")"
-	end
-	return true, "You have already rocked the vote!"
 end)
