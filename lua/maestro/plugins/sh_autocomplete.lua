721e8d40da43ea746525938c79e92aa320991677
DaaOtt <daaott@gmail.com>
2015-12-15 18:03:28 -0800

    Better chat autocomplete

--- lua/maestro/plugins/sh_autocomplete.lua
+++ lua/maestro/plugins/sh_autocomplete.lua
@@ -1,6 +1,6 @@
 --[[
     Note: This is chat autocomplete. Console autocomplete is in cl_commands.lua
-	If this breaks yell at http://facepunch.com/member.php?u=646169
+	Originally contributed by Fluffy (http://facepunch.com/member.php?u=646169)
 -]]
 if CLIENT then
 	function draw.AAText(text, font, x, y, color, align)
@@ -20,8 +20,8 @@ function ma:DrawSuggestions()
 		cy = cy + sh / 4 + 4
 		surface.SetFont('ChatFont')
 		for _, v in ipairs(self.sug) do
-			local tx, ty = surface.GetTextSize(v.command)
-			draw.AAText(v.command, "ChatFont", cx, cy, Color(255, 255, 255, 255))
+			local tx, ty = surface.GetTextSize(v)
+			draw.AAText(v, "ChatFont", cx, cy, Color(255, 255, 255, 255))
 			cy = cy + ty
 		end
 	end
@@ -46,8 +46,8 @@ function ma:GetSugCmds(message)
 	end
 end
 function ma:Autocomplete(s)
-	if string.match(s, "^[!][^ ]*$") and #self.sug > 0 then
-		return self.sug[1].command
+	if string.match(s, "^!") and #self.sug > 0 then
+		return self.sug[1]
 	end
 end
 function ma:ChatOpen(t)
@@ -56,8 +56,113 @@ end
 function ma:ChatClose()
 	self.c = false
 end
+local function escape(str)
+	return string.gsub(str, "([%(%)%.%%%+%-%*%?%[%^%$])", "%%%1")
+end
+local function autocomplete(str)
+	local base = "!"
+	str = string.sub(str, 2, -1)
+	local args = maestro.split(str, true)
+	local t = {}
+	if #args == 0 then
+		for k, v in pairs(maestro.commands) do
+			if maestro.rankget(maestro.userrank(LocalPlayer())).perms[k] then
+				table.insert(t, base .. k)
+			end
+		end
+	elseif #args == 1 then
+		for k, v in pairs(maestro.commands) do
+			if maestro.rankget(maestro.userrank(LocalPlayer())).perms[k] then
+				if string.sub(k, 1, #args[1]):lower() == args[1]:lower() then
+					table.insert(t, base .. k)
+				end
+			end
+		end
+	else
+		local cmd, types
+		for k, v in pairs(maestro.commands) do
+			if k:lower() == args[1]:lower() then
+				cmd = k
+				types = v.args
+			end
+		end
+		local params = table.Copy(args)
+		table.remove(params, 1)
+		if cmd then
+			local cnct = table.concat(args, " ", 2, #args - 1)
+			cnct = " " .. cnct .. " "
+			cnct = cnct:gsub("%s+", " ")
+			local typ = string.match(types[#params] or types[#types], "[^:]+")
+			if typ == "player" then
+				local plys = maestro.target(params[#params], LocalPlayer(), cmd)
+				for i = 1, #plys do
+					table.insert(t, base .. cmd .. cnct .. "\"" .. plys[i]:Nick() .. "\"")
+				end
+			elseif typ == "boolean" then
+				local options = {"true", "false", "t", "f", "1", "0", "yes", "no"}
+				for i = 1, #options do
+					if string.sub(options[i], 1, #args[#args]):lower() == args[#args]:lower() then
+						table.insert(t, base .. cmd .. cnct .. options[i])
+					end
+				end
+			elseif typ == "rank" then
+				local ranks = {}
+				local cr = maestro.rankget(maestro.userrank(LocalPlayer())).canrank
+				if cr then
+					ranks = maestro.targetrank(cr, maestro.userrank(LocalPlayer()))
+				end
+				for rank in pairs(ranks) do
+					if string.sub(rank, 1, #args[#args]):lower() == args[#args]:lower() then
+						table.insert(t, base .. cmd .. cnct .. rank)
+					end
+				end
+			elseif typ == "command" then
+				for cmd2 in pairs(maestro.commands) do
+					if string.sub(cmd2, 1, #args[#args]):lower() == args[#args]:lower() then
+						table.insert(t, base .. cmd .. cnct .. cmd2)
+					end
+				end
+			elseif typ == "sound" then
+				local input = tostring(args[#args])
+				input = input:gsub("\\", "/")
+				input = input:gsub("%.%.", "")
+				local path, name = input:match("(.*/)([^/]*)$")
+				path = path or ""
+				name = name or input
+				local files, folders = file.Find("sound/" .. input .. "*", "GAME")
+				if files and folders then
+					for i = 1, #files do
+						table.insert(t, base .. cmd .. cnct .. path .. files[i])
+					end
+					for i = 1, #folders do
+						if folders[i]:sub(1, #name) == name then
+							table.insert(t, base .. cmd .. cnct .. path .. folders[i])
+						end
+					end
+				end
+			elseif typ == "style" then
+				local options = {"primary", "success", "info", "warning", "danger"}
+				for i = 1, #options do
+					if string.sub(options[i], 1, #args[#args]):lower() == args[#args]:lower() then
+						table.insert(t, base .. cmd .. cnct .. options[i])
+					end
+				end
+			elseif typ == "team" then
+				for id, tab in pairs(team.GetAllTeams()) do
+					if string.sub(tab.Name or "", 1, #args[#args]):lower() == args[#args]:lower() then
+						table.insert(t, base .. cmd .. cnct .. tab.Name)
+					end
+				end
+			elseif types[#params] then
+				table.insert(t, base .. cmd .. cnct .. "<" .. types[#params] .. ">")
+			end
+		end
+	end
+	table.sort(t)
+	ma.sug = t
+end
 maestro.hook("HUDPaint", "cmdsuggest", function() ma:DrawSuggestions() end)
-maestro.hook("ChatTextChanged", "findsuggest", function(msg) ma:GetSugCmds(msg) end)
+maestro.hook("ChatTextChanged", "findsuggest", autocomplete)
 maestro.hook("StartChat", "plopenchat", function(t) ma:ChatOpen(t) end)
 maestro.hook("FinishChat", "plclosechat", function() ma:ChatClose() end)
-maestro.hook("OnChatTab", "autocomplete", function(s) ma:Autocomplete(s) end)
+maestro.hook("OnChatTab", "autocomplete", function(s) return ma:Autocomplete(s) end)
