a9174bf9c81afb3c66d4180f7767f0b24bd971cf
DaaOtt <daaott@gmail.com>
2015-12-19 12:34:39 -0800

    1.19.1: Redid trainfuck and cartoss

--- lua/maestro/plugins/sh_trainfuck.lua
+++ lua/maestro/plugins/sh_trainfuck.lua
@@ -13,16 +13,16 @@ maestro_cartoss_models[7] = "models/props_vehicles/car003b_physics.mdl"
 
 maestro.command("trainfuck", {"player:target"}, function(caller, targets)
 	if #targets == 0 then return true, "Query matched no players." end
-	
+
 	for i = 1, #targets do
 		local ply = targets[i]
-		
+
 		if not ply:Alive() then
 			if #targets == 1 then return true, "Player is dead!" end
 			continue
 		end
-		
-		local trainPos, trainDirection = ply:GetPos() + ply:GetForward() * 1000 + Vector(0, 0, 125), ply:GetForward() * -1
+
+		local trainPos, trainDirection = ply:GetPos() + ply:GetForward() * 2000 + Vector(0, 0, 100), ply:GetForward() * -1
 		local trainModel = maestro_trainfuck_models[math.random(1, #maestro_trainfuck_models)]
 		local train = ents.Create("prop_physics")
 		train:SetModel(trainModel)
@@ -30,47 +30,50 @@ maestro.command("trainfuck", {"player:target"}, function(caller, targets)
 		train:SetPos(trainPos)
 		train:Spawn()
 		train:Activate()
-		train:EmitSound("ambient/alarms/train_horn2.wav", 100, 100)
-		local phys = train:GetPhysicsObject()
-		phys:SetVelocity(trainDirection * 100000)
-		
+		train:EmitSound("ambient/alarms/train_horn2.wav", 511, 100)
+		local obj = train:GetPhysicsObject()
+		if IsValid(obj) then
+			obj:EnableGravity(false)
+			obj:EnableCollisions(false)
+			obj:SetVelocity(trainDirection * 100000)
+		end
+		timer.Simple(0.6, function()
+			local dmg = DamageInfo()
+			dmg:AddDamage(2^31 -1)
+			dmg:SetDamageForce(trainDirection * 100000)
+			dmg:SetInflictor(train)
+			dmg:SetAttacker(train)
+			targets[i]:TakeDamageInfo(dmg)
+		end)
+
 		timer.Simple(3, function()
 			train:Remove()
 		end)
 	end
-	
+
 	return false, "trainfucked %1"
 end, [[
 Throws a train at the target.]])
 
-maestro.command("cartoss", {"player:target"}, function(caller, targets)
-	if #targets == 0 then return true, "Query matched no players." end
-	
-	for i = 1, #targets do
-		local ply = targets[i]
-		
-		if not ply:Alive() then
-			if #targets == 1 then return true, "Player is dead!" end
-			continue
-		end
-		
-		local carPos, carDirection = ply:GetPos() + ply:GetForward() * 1000 + Vector(0, 0, 75), ply:GetForward() * -1
-		local carModel = maestro_cartoss_models[math.random(1, #maestro_cartoss_models)]
-		local car = ents.Create("prop_physics")
-		car:SetModel(carModel)
-		car:SetAngles(carDirection:Angle() + Angle(0, 270, 0))
-		car:SetPos(carPos)
-		car:Spawn()
-		car:Activate()
-		local phys = car:GetPhysicsObject()
-		phys:SetVelocity(carDirection * 100000)
-		
-		timer.Simple(3, function()
-			car:Remove()
-		end)
-	end
-	
-	return false, "cartossed %1"
+maestro.command("cartoss", {}, function(ply)
+	local carPos, carDirection = ply:GetShootPos(), ply:EyeAngles():Forward()
+	local carModel = maestro_cartoss_models[math.random(1, #maestro_cartoss_models)]
+	local car = ents.Create("prop_physics")
+	car:SetModel(carModel)
+	car:SetAngles(carDirection:Angle())
+	car:SetPos(carPos)
+	car:Spawn()
+	car:Activate()
+	car:SetOwner(ply)
+	car:SetMaxHealth(2^31 - 1) --don't ask
+	local phys = car:GetPhysicsObject()
+	phys:SetVelocity(carDirection * 10000)
+
+	timer.Simple(3, function()
+		car:Remove()
+	end)
+
+	return false, "threw a car"
 end, [[
-Throws a car at the target.]])
---Plugin by FluffyXVI
+Throws a car.]])
+--Plugin originally by FluffyXVI
