89c02d17d96d62064c0d500bb54184cd76eb268f
DaaOtt <daaott@gmail.com>
2016-04-30 22:14:29 -0700

    Consolidated autocomplete

---
+++ lua/maestro/sh_autocomplete.lua
@@ -0,0 +1,121 @@
+function maestro.autocomplete(base, str)
+	base = base .. " "
+	str = string.sub(str, 2, -1)
+	local args = maestro.split(str, true)
+	local t = {}
+	if #args == 0 then
+		for k, v in pairs(maestro.commands) do
+			if maestro.rankget(maestro.userrank(LocalPlayer())).perms[k] then
+				table.insert(t, base .. k)
+			end
+		end
+		for k, v in pairs(maestro.commandaliases) do
+			if maestro.rankget(maestro.userrank(LocalPlayer())).perms[v] then
+				table.insert(t, base .. k)
+			end
+		end
+	elseif #args == 1 then
+		for k, v in pairs(maestro.commands) do
+			if maestro.rankget(maestro.userrank(LocalPlayer())).perms[k] then
+				if string.sub(k, 1, #args[1]):lower() == args[1]:lower() then
+					table.insert(t, base .. k)
+				end
+			end
+		end
+		for k, v in pairs(maestro.commandaliases) do
+			if maestro.rankget(maestro.userrank(LocalPlayer())).perms[v] then
+				if string.sub(k, 1, #args[1]):lower() == args[1]:lower() then
+					table.insert(t, base .. k)
+				end
+			end
+		end
+	else
+		local cmd, types
+		for k, v in pairs(maestro.commands) do
+			if k:lower() == args[1]:lower() then
+				cmd = k
+				types = v.args
+			end
+		end
+		for k, v in pairs(maestro.commandaliases) do
+			if k:lower() == args[1]:lower() then
+				cmd = k
+				types = maestro.commands[v].args
+			end
+		end
+		local params = table.Copy(args)
+		table.remove(params, 1)
+		if cmd then
+			cmd = maestro.commandaliases[cmd] or cmd
+			local cnct = table.concat(args, " ", 2, #args - 1)
+			cnct = " " .. cnct .. " "
+			cnct = cnct:gsub("%s+", " ")
+			local typ = string.match(types[#params] or types[#types], "[^:]+")
+			if typ == "player" then
+				local plys = maestro.target(params[#params], LocalPlayer(), cmd)
+				for i = 1, #plys do
+					table.insert(t, base .. cmd .. cnct .. "\"" .. plys[i]:Nick() .. "\"")
+				end
+			elseif typ == "boolean" then
+				local options = {"true", "false", "t", "f", "1", "0", "yes", "no"}
+				for i = 1, #options do
+					if string.sub(options[i], 1, #args[#args]):lower() == args[#args]:lower() then
+						table.insert(t, base .. cmd .. cnct .. options[i])
+					end
+				end
+			elseif typ == "rank" then
+				local ranks = {}
+				local cr = maestro.rankget(maestro.userrank(LocalPlayer())).canrank
+				if cr then
+					ranks = maestro.targetrank(cr, maestro.userrank(LocalPlayer()))
+				end
+				for rank in pairs(ranks) do
+					if string.sub(rank, 1, #args[#args]):lower() == args[#args]:lower() then
+						table.insert(t, base .. cmd .. cnct .. rank)
+					end
+				end
+			elseif typ == "command" then
+				for cmd2 in pairs(maestro.commands) do
+					if string.sub(cmd2, 1, #args[#args]):lower() == args[#args]:lower() then
+						table.insert(t, base .. cmd .. cnct .. cmd2)
+					end
+				end
+			elseif typ == "sound" then
+				local input = tostring(args[#args])
+				input = input:gsub("\\", "/")
+				input = input:gsub("%.%.", "")
+				local path, name = input:match("(.*/)([^/]*)$")
+				path = path or ""
+				name = name or input
+				local files, folders = file.Find("sound/" .. input .. "*", "GAME")
+				if files and folders then
+					for i = 1, #files do
+						table.insert(t, base .. cmd .. cnct .. path .. files[i])
+					end
+					for i = 1, #folders do
+						if folders[i]:sub(1, #name) == name then
+							table.insert(t, base .. cmd .. cnct .. path .. folders[i])
+						end
+					end
+				end
+			elseif typ == "style" then
+				local options = {"primary", "success", "info", "warning", "danger"}
+				for i = 1, #options do
+					if string.sub(options[i], 1, #args[#args]):lower() == args[#args]:lower() then
+						table.insert(t, base .. cmd .. cnct .. options[i])
+					end
+				end
+			elseif typ == "team" then
+				for id, tab in pairs(team.GetAllTeams()) do
+					if string.sub(tab.Name or "", 1, #args[#args]):lower() == args[#args]:lower() then
+						table.insert(t, base .. cmd .. cnct .. tab.Name)
+					end
+				end
+			elseif types[#params] then
+				table.insert(t, base .. cmd .. cnct .. "<" .. types[#params] .. ">")
+			end
+		end
+	end
+	table.sort(t)
+	return t
+end
